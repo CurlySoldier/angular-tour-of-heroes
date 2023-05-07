ed121d2d5abdd7258a18aae3e5f3a59ce0ffffd3
CurlySoldier <73424198+CurlySoldier@users.noreply.github.com>
2023-05-07 11:05:46 +0200

    feat: Added multi page navigation

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageservice: MessageService) { }
 
-  getHeroes(): Hero[]{
-    return HEROES;
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageservice.add('HeroService: fetched heroes');
+    return heroes;
   }
 }
